f9841268bc5e2c919bdd8a8a96740f9d60616969
Shift <shift@laravelshift.com>
2024-07-12 17:18:05 +0000

    Re-register scheduled commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Schedule::command('tier4:notify-staff-overdue-meetings')->dailyAt('07:00');
+Schedule::command('tier4:trim-activity-table')->sundays()->at('04:00');
